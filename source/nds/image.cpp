d149362182e6de2185ae87f5276268b0c4dec6b8
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2025-10-01 18:00:11 -0400

    Update image.cpp (again)

--- source/nds/image.cpp
+++ source/nds/image.cpp
@@ -1,8 +1,4 @@
 #include "../scratch/image.hpp"
-#include <nf_lib.h>
-//This image stuff is going to be tough; the DS uses memory "banks" of fixed sizes that might not be the best use of space when it comes to images.
-//The vram banks add up to 656 KB.
-
 Image::Image(std::string filePath) : width(0), height(0), scale(1.0), opacity(1.0), rotation(0.0) {
 }
 
@@ -10,25 +6,9 @@ Image::~Image() {
 }
 
 void Image::render(double xPos, double yPos, bool centered) {
-	float newX = xPos; // I think we should move away from doubles and use floats instead to save memory and improve performance since the DS has no fpu.
-	float newY = yPos;
-	
-	if(centered){
-		newX += getWidth() / 2;
-		newY += getHeight() / 2;
-	}
-
-	//NF_load16BitsImage();//TODO
 }
 
 bool Image::loadImageFromFile(std::string filePath, bool fromScratchProject) {//TODO
-	NF_CreateSprite( 
-		0, // Sprite Id (0 – 127)
-		0, // Gfx slot (0 – 127)
-		0, // Palette slot (0 – 15)
-		0, // X coordinate
-		0 // Y coordinate
-	);
     return false;
 }
 
