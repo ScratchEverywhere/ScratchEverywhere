d429265f61053d71a9a316c59cf0e249dd6fdbdb
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2025-10-01 11:27:39 -0400

    Update image.cpp render function

--- source/nds/image.cpp
+++ source/nds/image.cpp
@@ -1,4 +1,7 @@
 #include "../scratch/image.hpp"
+#include <gl2d.h>
+//This image stuff is going to be tough; the DS uses memory "banks" of fixed sizes that might not be the best use of space when it comes to images.
+//The vram banks add up to 656 KB.
 
 Image::Image(std::string filePath) : width(0), height(0), scale(1.0), opacity(1.0), rotation(0.0) {
 }
@@ -7,6 +10,15 @@ Image::~Image() {
 }
 
 void Image::render(double xPos, double yPos, bool centered) {
+	float newX = xPos; // I think we should move away from doubles and use floats instead to save memory and improve performance since the DS has no fpu.
+	float newY = yPos;
+	
+	if(centered){
+		newX += getWidth() / 2;
+		newY += getHeight() /2;
+	}
+
+	
 }
 
 bool Image::loadImageFromFile(std::string filePath, bool fromScratchProject) {
