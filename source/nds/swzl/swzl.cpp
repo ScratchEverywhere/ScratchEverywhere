d0afc856ce98a6b68dcc3406be3f30f0ad83e0a0
Cammy43 <112432153+Cammy43@users.noreply.github.com>
2025-10-03 15:07:26 -0400

    Update swzl.cpp with scaling feature (again i think)

--- source/nds/swzl/swzl.cpp
+++ source/nds/swzl/swzl.cpp
@@ -4,7 +4,7 @@ void stbToSwzl(uint8_t *img, swzlImg *swzl)
 { // TODO
 }
 
-void drawSwzl(swzlImg *img, uint16_t x, uint16_t y, bool centered = false)
+void drawSwzl(swzlImg *img, uint16_t x, uint16_t y, bool centered = false)// this is slow, and won't stay for long
 {// swizzle magic
     glBegin2D();
     uint8_t r,g,b;
@@ -14,18 +14,21 @@ void drawSwzl(swzlImg *img, uint16_t x, uint16_t y, bool centered = false)
         {
             for (uint8_t px = 0; px < img.w; i++)
             {
-                swzlPixel thisPixel = img.img[px + (py * img.w)];
+                swzlPixel thisPixel = img.img[w + (h * img.w)];
                 glDrawPoint(px + x, py + y, convertSwzlPixel(thisPixel,img.palette));
             }
         }
     }
     else
     {
-
+		for (uint8_t py = 0; py < img.h; i++)
+        {
+            for (uint8_t px = 0; px < img.w; i++)
+            {
+                swzlPixel thisPixel = img.img[w + (h * img.w)];
+                glBoxFill((px * scale) + x, (py * scale) + y, (px * scale) + x + scale, (py * scale) + y + scale, convertSwzlPixel(thisPixel,img.palette));
+            }
+        }
     }
     glEnd2D();
-}
-
-int main(){
-    return 0;
 }
