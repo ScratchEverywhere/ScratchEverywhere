05c80a7680db1b8f1d515f2d3503e87209cf3147
poipole807 <poipole807@gmail.com>
2025-11-14 22:13:50 -0500

    Fix pen color clamping for negative values (#458)

--- source/scratch/color.hpp
+++ source/scratch/color.hpp
@@ -20,20 +20,20 @@ struct ColorRGB {
     float b;
 };
 
-inline ColorRGB HSB2RGB(const Color &color) {
+inline ColorRGB CSB2RGB(const Color &color) {
     const float C = (color.saturation / 100) * (color.brightness / 100);
-    const float X = C * (1 - fabs(fmod(color.hue / 60.0, 2) - 1));
+    const float X = C * (1 - fabs(fmod(color.hue * 0.06, 2) - 1));
     const float m = color.brightness / 100 - C;
 
-    if (color.hue >= 000 && color.hue < 060) return {(C + m) * 255, (X + m) * 255, m * 255};
-    if (color.hue >= 060 && color.hue < 120) return {(X + m) * 255, (C + m) * 255, m * 255};
-    if (color.hue >= 120 && color.hue < 180) return {m * 255, (C + m) * 255, (X + m) * 255};
-    if (color.hue >= 180 && color.hue < 240) return {m * 255, (X + m) * 255, (C + m) * 255};
-    if (color.hue >= 240 && color.hue < 300) return {(X + m) * 255, m * 255, (C + m) * 255};
+    if (color.hue >= 0 && color.hue < 50.0 / 3) return {(C + m) * 255, (X + m) * 255, m * 255};
+    if (color.hue >= 50.0 / 3 && color.hue < 100.0 / 3) return {(X + m) * 255, (C + m) * 255, m * 255};
+    if (color.hue >= 100.0 / 3 && color.hue < 150.0 / 3) return {m * 255, (C + m) * 255, (X + m) * 255};
+    if (color.hue >= 150.0 / 3 && color.hue < 200.0 / 3) return {m * 255, (X + m) * 255, (C + m) * 255};
+    if (color.hue >= 200.0 / 3 && color.hue < 250.0 / 3) return {(X + m) * 255, m * 255, (C + m) * 255};
     return {(C + m) * 255, m * 255, (X + m) * 255};
 }
 
-inline Color RGB2HSB(const ColorRGB &color) {
+inline Color RGB2CSB(const ColorRGB &color) {
     const float r = color.r / 255.0f;
     const float g = color.g / 255.0f;
     const float b = color.b / 255.0f;
@@ -44,9 +44,9 @@ inline Color RGB2HSB(const ColorRGB &color) {
     const float s = (cmax == 0) ? 0 : (diff / cmax) * 100;
 
     if (diff == 0) goto end;
-    if (cmax == r) return {static_cast<float>(fmod(60 * ((g - b) / diff) + 360, 360)), s, cmax * 100};
-    if (cmax == g) return {static_cast<float>(fmod(60 * ((b - r) / diff) + 120, 360)), s, cmax * 100};
-    if (cmax == b) return {static_cast<float>(fmod(60 * ((r - g) / diff) + 240, 360)), s, cmax * 100};
+    if (cmax == r) return {static_cast<float>(fmod(60 * ((g - b) / diff) + 360, 360)) * 100 / 360, s, cmax * 100};
+    if (cmax == g) return {static_cast<float>(fmod(60 * ((b - r) / diff) + 120, 360)) * 100 / 360, s, cmax * 100};
+    if (cmax == b) return {static_cast<float>(fmod(60 * ((r - g) / diff) + 240, 360)) * 100 / 360, s, cmax * 100};
 
 end:
     return {0, s, cmax * 100};
