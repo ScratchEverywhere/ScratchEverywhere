355576edfd96a0aba85045ccc3b2b7a6ffdaee23
Grady Link <github@grady.link>
2025-11-23 02:15:43 -0800

    fix: compile error from merge

--- source/scratch/blocks/blockUtils.hpp
+++ source/scratch/blocks/blockUtils.hpp
@@ -15,9 +15,9 @@ BlockResult nopBlock(Block &block, Sprite *sprite, bool *withoutScreenRefresh, b
     Value category##_##id##_(Block &block, Sprite *sprite);                                                            \
     static uint8_t category##_##id##_reg_ = (BlockExecutor::valueHandlers[#category "_" #id] = category##_##id##_, 0); \
     Value category##_##id##_(Block &block, Sprite *sprite)
-#define SCRATCH_REPORTER_BLOCK_OPCODE(opcode)                                                       \
-    Value opcode##_(Block &block, Sprite *sprite);                                                  \
-    static uint8_t category##_##id##_reg_ = (BlockExecutor::valueHandlers[#opcode] = opcode##_, 0); \
+#define SCRATCH_REPORTER_BLOCK_OPCODE(opcode)                                              \
+    Value opcode##_(Block &block, Sprite *sprite);                                         \
+    static uint8_t opcode##_reg_ = (BlockExecutor::valueHandlers[#opcode] = opcode##_, 0); \
     Value opcode##_(Block &block, Sprite *sprite)
 #define SCRATCH_SHADOW_BLOCK(opcode, fieldId)                                                                \
     Value opcode##_(Block &block, Sprite *sprite) { return Value(Scratch::getFieldValue(block, #fieldId)); } \
