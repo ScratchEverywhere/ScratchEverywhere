425f83550d67208980be4d72b10459ff1a981313
Grady Link <github@grady.link>
2025-11-23 02:30:13 -0800

    fix(runtime): mistakes in last commit

--- source/scratch/blocks/blockUtils.hpp
+++ source/scratch/blocks/blockUtils.hpp
@@ -5,16 +5,16 @@
 
 BlockResult nopBlock(Block &block, Sprite *sprite, bool *withoutScreenRefresh, bool fromRepeat);
 
-#define SCRATCH_BLOCK(category, id)                                                                                           \
-    BlockResult block_category##_##id##_(Block &block, Sprite *sprite, bool *withoutScreenRefresh, bool fromRepeat);          \
-    static uint8_t block_category##_##id##_reg_ = (BlockExecutor::handlers[#category "_" #id] = block_category##_##id##_, 0); \
-    BlockResult block_category##_##id##_(Block &block, Sprite *sprite, bool *withoutScreenRefresh, bool fromRepeat)
+#define SCRATCH_BLOCK(category, id)                                                                                               \
+    BlockResult block_##category##_##id##_(Block &block, Sprite *sprite, bool *withoutScreenRefresh, bool fromRepeat);            \
+    static uint8_t block_##category##_##id##_reg_ = (BlockExecutor::handlers[#category "_" #id] = block_##category##_##id##_, 0); \
+    BlockResult block_##category##_##id##_(Block &block, Sprite *sprite, bool *withoutScreenRefresh, bool fromRepeat)
 #define SCRATCH_BLOCK_NOP(category, id) \
-    static uint8_t block_category##_##id##_reg_ = (BlockExecutor::handlers[#category "_" #id] = nopBlock, 0);
-#define SCRATCH_REPORTER_BLOCK(category, id)                                                                                       \
-    Value block_category##_##id##_(Block &block, Sprite *sprite);                                                                  \
-    static uint8_t block_category##_##id##_reg_ = (BlockExecutor::valueHandlers[#category "_" #id] = block_category##_##id##_, 0); \
-    Value block_category##_##id##_(Block &block, Sprite *sprite)
+    static uint8_t block_##category##_##id##_reg_ = (BlockExecutor::handlers[#category "_" #id] = nopBlock, 0);
+#define SCRATCH_REPORTER_BLOCK(category, id)                                                                                           \
+    Value block_##category##_##id##_(Block &block, Sprite *sprite);                                                                    \
+    static uint8_t block_##category##_##id##_reg_ = (BlockExecutor::valueHandlers[#category "_" #id] = block_##category##_##id##_, 0); \
+    Value block_##category##_##id##_(Block &block, Sprite *sprite)
 #define SCRATCH_REPORTER_BLOCK_OPCODE(opcode)                                                              \
     Value block_##opcode##_(Block &block, Sprite *sprite);                                                 \
     static uint8_t block_##opcode##_reg_ = (BlockExecutor::valueHandlers[#opcode] = block_##opcode##_, 0); \
