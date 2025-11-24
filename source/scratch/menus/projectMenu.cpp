1728b4fac994be00fea6bb5ff07024e503f4ced2
Fancy2209 <paulo.medeiros1302@gmail.com>
2025-11-24 21:28:11 -0100

    Update ProjectMenu::init() for PS3

--- source/scratch/menus/projectMenu.cpp
+++ source/scratch/menus/projectMenu.cpp
@@ -116,6 +116,8 @@ void ProjectMenu::init() {
         noProjectInfo->setText("Put Scratch projects in sd:/scratch-ds !");
 #elif defined(__PS4__)
         noProjectInfo->setText("Put Scratch projects in /data/scratch-ps4 !");
+#elif defined(__PS3__)
+        noProjectInfo->setText("Put Scratch projects in /dev_hdd0/data/scratch-ps3 !");
 #else
         noProjectInfo->setText("Put Scratch projects in the scratch-everywhere folder!");
 #endif
