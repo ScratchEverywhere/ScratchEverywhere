cde92fb791acb4256af85642b3eabcafdeab22e0
Grady Link <github@grady.link>
2025-11-29 22:53:14 -0800

    refactor: switch os from class to namespace

--- source/os.hpp
+++ source/os.hpp
@@ -50,283 +50,282 @@ class Timer {
     bool hasElapsedAndRestart(int ms);
 };
 
-class OS {
-  public:
-    static inline std::string getFilesystemRootPrefix() {
+namespace OS {
+inline std::string getFilesystemRootPrefix() {
 #ifdef __WIIU__
-        return std::string(WHBGetSdCardMountPath());
+    return std::string(WHBGetSdCardMountPath());
 #elif defined(__SWITCH__)
-        return "";
+    return "";
 #elif defined(WII)
-        return "";
+    return "";
 #elif defined(GAMECUBE)
-        return "";
+    return "";
 #elif defined(VITA)
-        return "ux0:";
+    return "ux0:";
 #elif defined(__PS4__)
-        return "";
+    return "";
 #elif defined(__3DS__)
-        return "sdmc:";
+    return "sdmc:";
 #elif defined(__EMSCRIPTEN__)
-        return "";
+    return "";
 #elif defined(__NDS__)
-        return isDSi() ? "sd:" : "fat:";
+    return isDSi() ? "sd:" : "fat:";
 #else
-        return "";
+    return "";
 #endif
-    }
+}
 
-    /**
-     * Gets the location of the current device's Scratch data folder.
-     * This is where the user should put their .sb3 Scratch projects.
-     * This is also where all the extra data of the app lies (custom controls data, etc).
-     * @return The string of the current device's Scratch data folder.
-     */
-    static inline std::string getScratchFolderLocation() {
-        const std::string prefix = getFilesystemRootPrefix();
+/**
+ * Gets the location of the current device's Scratch data folder.
+ * This is where the user should put their .sb3 Scratch projects.
+ * This is also where all the extra data of the app lies (custom controls data, etc).
+ * @return The string of the current device's Scratch data folder.
+ */
+inline std::string getScratchFolderLocation() {
+    const std::string prefix = getFilesystemRootPrefix();
 #ifdef __WIIU__
-        return prefix + "/wiiu/scratch-wiiu/";
+    return prefix + "/wiiu/scratch-wiiu/";
 #elif defined(__SWITCH__)
-        return "/switch/scratch-nx/";
+    return "/switch/scratch-nx/";
 #elif defined(WII)
-        return "/apps/scratch-wii/";
+    return "/apps/scratch-wii/";
 #elif defined(GAMECUBE)
-        return "/scratch-gamecube/";
+    return "/scratch-gamecube/";
 #elif defined(VITA)
-        return prefix + "data/scratch-vita/";
+    return prefix + "data/scratch-vita/";
 #elif defined(__PS4__)
-        return "/data/scratch-ps4/";
+    return "/data/scratch-ps4/";
 #elif defined(__3DS__)
-        return prefix + "/3ds/scratch-everywhere/";
+    return prefix + "/3ds/scratch-everywhere/";
 #elif defined(__EMSCRIPTEN__)
-        return "/scratch-everywhere/";
+    return "/scratch-everywhere/";
 #elif defined(__NDS__)
-        return prefix + "/scratch-ds/";
+    return prefix + "/scratch-ds/";
 #else
-        return "scratch-everywhere/";
+    return "scratch-everywhere/";
 #endif
-    }
+}
 
-    /**
-     * Gets the location of the `RomFS`, the embedded filesystem within the executable.
-     * This function should be used whenever you need to load an asset from say, the `gfx` folder.
-     * @return The location of the RomFS. On OGC, Switch, Wii U, and 3DS, this is `romfs:/`. everywhere else will be an empty string.
-     */
-    static inline std::string getRomFSLocation() {
+/**
+ * Gets the location of the `RomFS`, the embedded filesystem within the executable.
+ * This function should be used whenever you need to load an asset from say, the `gfx` folder.
+ * @return The location of the RomFS. On OGC, Switch, Wii U, and 3DS, this is `romfs:/`. everywhere else will be an empty string.
+ */
+inline std::string getRomFSLocation() {
 #if defined(__WIIU__) || defined(__OGC__) || defined(__SWITCH__) || defined(__3DS__)
-        return "romfs:/";
+    return "romfs:/";
 #elif defined(__EMSCRIPTEN__)
-        return "/romfs/";
+    return "/romfs/";
 #elif defined(__PS4__)
-        return "/app0/";
+    return "/app0/";
 #else
-        return "";
+    return "";
 #endif
-    }
+}
 
-    /**
-     * Get the current platform that's running the app.
-     * @return The string of the current platform. `3DS`, `Wii`, etc.
-     */
-    static inline std::string getPlatform() {
+/**
+ * Get the current platform that's running the app.
+ * @return The string of the current platform. `3DS`, `Wii`, etc.
+ */
+inline std::string getPlatform() {
 #if defined(__3DS__)
-        return "3DS";
+    return "3DS";
 #elif defined(__WIIU__)
-        return "Wii U";
+    return "Wii U";
 #elif defined(__PC__)
-        return "PC";
+    return "PC";
 #elif defined(GAMECUBE)
-        return "GameCube";
+    return "GameCube";
 #elif defined(WII)
-        return "Wii";
+    return "Wii";
 #elif defined(__SWITCH__)
-        return "Switch";
+    return "Switch";
 #elif defined(VITA)
-        return "Vita";
+    return "Vita";
 #elif defined(__NDS__)
-        return "DS";
+    return "DS";
 #elif defined(__EMSCRIPTEN__)
-        return "WASM";
+    return "WASM";
 #elif defined(__PS4__)
-        return "PS4";
+    return "PS4";
 #elif defined(__PSP__)
-        return "PSP";
+    return "PSP";
 #else
-        return "Unknown";
+    return "Unknown";
 #endif
-    }
+}
 
-    /**
-     * Function to detect whether the platform is a New 3DS.
-     * @return `true` on New 3DS, `false` everywhere else.
-     */
-    static inline bool isNew3DS() {
+/**
+ * Function to detect whether the platform is a New 3DS.
+ * @return `true` on New 3DS, `false` everywhere else.
+ */
+inline bool isNew3DS() {
 #ifdef __3DS__
-        bool out = false;
-        APT_CheckNew3DS(&out);
-        return out;
+    bool out = false;
+    APT_CheckNew3DS(&out);
+    return out;
 #endif
-        return false;
-    }
+    return false;
+}
 
-    /**
-     * Function to detect whether the platform is a DSi.
-     * @return `true` on DSi, `false` everywhere else.
-     */
-    static inline bool isDSi() {
+/**
+ * Function to detect whether the platform is a DSi.
+ * @return `true` on DSi, `false` everywhere else.
+ */
+static inline bool isDSi() {
 #ifdef __NDS__
-        return isDSiMode();
+    return isDSiMode();
 #endif
-        return false;
-    }
+    return false;
+}
+/**
+ * Create a directory.
+ * @param path The path of the directory to create.
+ */
+void createDirectory(const std::string &path);
+
+/**
+ * Remove a directory recursively.
+ * @param path The path of the directory to remove.
+ */
+void removeDirectory(const std::string &path);
+
+/**
+ * Check if a file exists.
+ * @param path The path of the file to check.
+ * @return `true` if the file exists, `false` otherwise.
+ */
+bool fileExists(const std::string &path);
+
+/**
+ * Get the parent path of a given path.
+ * @param path The path of the file or directory.
+ * @return The parent path of the given path.
+ */
+std::string parentPath(const std::string &path);
+
+class FilesystemError : public std::runtime_error {
+  protected:
+    std::string path1;
+    std::string path2;
+    int errorCode;
+
+  public:
     /**
-     * Create a directory.
-     * @param path The path of the directory to create.
+     * Construct a FilesystemError with a message and single path.
+     * @param message The error message.
+     * @param p The path involved in the error.
+     * @param ec The system error code (errno).
      */
-    static void createDirectory(const std::string &path);
-
-    /**
-     * Remove a directory recursively.
-     * @param path The path of the directory to remove.
-     */
-    static void removeDirectory(const std::string &path);
-
-    /**
-     * Check if a file exists.
-     * @param path The path of the file to check.
-     * @return `true` if the file exists, `false` otherwise.
-     */
-    static bool fileExists(const std::string &path);
-
-    /**
-     * Get the parent path of a given path.
-     * @param path The path of the file or directory.
-     * @return The parent path of the given path.
-     */
-    static std::string parentPath(const std::string &path);
-
-    class FilesystemError : public std::runtime_error {
-      protected:
-        std::string path1;
-        std::string path2;
-        int errorCode;
-
-      public:
-        /**
-         * Construct a FilesystemError with a message and single path.
-         * @param message The error message.
-         * @param p The path involved in the error.
-         * @param ec The system error code (errno).
-         */
-        FilesystemError(const std::string &message, const std::string &p, int ec = 0)
-            : std::runtime_error(message), path1(p), path2(""), errorCode(ec) {}
-
-        /**
-         * Construct a FilesystemError with a message and two paths.
-         * @param message The error message.
-         * @param p1 The first path involved in the error.
-         * @param p2 The second path involved in the error.
-         * @param ec The system error code (errno).
-         */
-        FilesystemError(const std::string &message, const std::string &p1, const std::string &p2, int ec = 0)
-            : std::runtime_error(message), path1(p1), path2(p2), errorCode(ec) {}
-
-        /**
-         * Get the first path involved in the error.
-         * @return The first path.
-         */
-        const std::string &path() const noexcept { return path1; }
-
-        /**
-         * Get the first path involved in the error.
-         * @return The first path.
-         */
-        const std::string &getPath1() const noexcept { return path1; }
-
-        /**
-         * Get the second path involved in the error.
-         * @return The second path.
-         */
-        const std::string &getPath2() const noexcept { return path2; }
-
-        /**
-         * Get the system error code.
-         * @return The error code.
-         */
-        int getErrorCode() const noexcept { return errorCode; }
-
-        virtual ~FilesystemError() = default;
-    };
+    FilesystemError(const std::string &message, const std::string &p, int ec = 0)
+        : std::runtime_error(message), path1(p), path2(""), errorCode(ec) {}
 
     /**
-     * Exception thrown when a directory does not exist.
+     * Construct a FilesystemError with a message and two paths.
+     * @param message The error message.
+     * @param p1 The first path involved in the error.
+     * @param p2 The second path involved in the error.
+     * @param ec The system error code (errno).
      */
-    class DirectoryNotFound : public FilesystemError {
-      public:
-        DirectoryNotFound(const std::string &p, int ec = 0)
-            : FilesystemError("Directory not found: " + p, p, ec) {}
-    };
+    FilesystemError(const std::string &message, const std::string &p1, const std::string &p2, int ec = 0)
+        : std::runtime_error(message), path1(p1), path2(p2), errorCode(ec) {}
 
     /**
-     * Exception thrown when a file does not exist.
+     * Get the first path involved in the error.
+     * @return The first path.
      */
-    class FileNotFound : public FilesystemError {
-      public:
-        FileNotFound(const std::string &p, int ec = 0)
-            : FilesystemError("File not found: " + p, p, ec) {}
-    };
+    const std::string &path() const noexcept { return path1; }
 
     /**
-     * Exception thrown when a path is not a directory.
+     * Get the first path involved in the error.
+     * @return The first path.
      */
-    class NotADirectory : public FilesystemError {
-      public:
-        NotADirectory(const std::string &p, int ec = 0)
-            : FilesystemError("Path is not a directory: " + p, p, ec) {}
-    };
+    const std::string &getPath1() const noexcept { return path1; }
 
     /**
-     * Exception thrown when a path is not a file.
+     * Get the second path involved in the error.
+     * @return The second path.
      */
-    class NotAFile : public FilesystemError {
-      public:
-        NotAFile(const std::string &p, int ec = 0)
-            : FilesystemError("Path is not a file: " + p, p, ec) {}
-    };
+    const std::string &getPath2() const noexcept { return path2; }
 
     /**
-     * Exception thrown when failing to create a directory.
+     * Get the system error code.
+     * @return The error code.
      */
-    class DirectoryCreationFailed : public FilesystemError {
-      public:
-        DirectoryCreationFailed(const std::string &p, int ec = 0)
-            : FilesystemError("Failed to create directory: " + p, p, ec) {}
-    };
+    int getErrorCode() const noexcept { return errorCode; }
 
-    /**
-     * Exception thrown when failing to remove a directory.
-     */
-    class DirectoryRemovalFailed : public FilesystemError {
-      public:
-        DirectoryRemovalFailed(const std::string &p, int ec = 0)
-            : FilesystemError("Failed to remove directory: " + p, p, ec) {}
-    };
-
-    /**
-     * Exception thrown when failing to remove a file.
-     */
-    class FileRemovalFailed : public FilesystemError {
-      public:
-        FileRemovalFailed(const std::string &p, int ec = 0)
-            : FilesystemError("Failed to remove file: " + p, p, ec) {}
-    };
-
-    /**
-     * Exception thrown when failing to open a directory.
-     */
-    class DirectoryOpenFailed : public FilesystemError {
-      public:
-        DirectoryOpenFailed(const std::string &p, int ec = 0)
-            : FilesystemError("Failed to open directory: " + p, p, ec) {}
-    };
+    virtual ~FilesystemError() = default;
 };
+
+/**
+ * Exception thrown when a directory does not exist.
+ */
+class DirectoryNotFound : public FilesystemError {
+  public:
+    DirectoryNotFound(const std::string &p, int ec = 0)
+        : FilesystemError("Directory not found: " + p, p, ec) {}
+};
+
+/**
+ * Exception thrown when a file does not exist.
+ */
+class FileNotFound : public FilesystemError {
+  public:
+    FileNotFound(const std::string &p, int ec = 0)
+        : FilesystemError("File not found: " + p, p, ec) {}
+};
+
+/**
+ * Exception thrown when a path is not a directory.
+ */
+class NotADirectory : public FilesystemError {
+  public:
+    NotADirectory(const std::string &p, int ec = 0)
+        : FilesystemError("Path is not a directory: " + p, p, ec) {}
+};
+
+/**
+ * Exception thrown when a path is not a file.
+ */
+class NotAFile : public FilesystemError {
+  public:
+    NotAFile(const std::string &p, int ec = 0)
+        : FilesystemError("Path is not a file: " + p, p, ec) {}
+};
+
+/**
+ * Exception thrown when failing to create a directory.
+ */
+class DirectoryCreationFailed : public FilesystemError {
+  public:
+    DirectoryCreationFailed(const std::string &p, int ec = 0)
+        : FilesystemError("Failed to create directory: " + p, p, ec) {}
+};
+
+/**
+ * Exception thrown when failing to remove a directory.
+ */
+class DirectoryRemovalFailed : public FilesystemError {
+  public:
+    DirectoryRemovalFailed(const std::string &p, int ec = 0)
+        : FilesystemError("Failed to remove directory: " + p, p, ec) {}
+};
+
+/**
+ * Exception thrown when failing to remove a file.
+ */
+class FileRemovalFailed : public FilesystemError {
+  public:
+    FileRemovalFailed(const std::string &p, int ec = 0)
+        : FilesystemError("Failed to remove file: " + p, p, ec) {}
+};
+
+/**
+ * Exception thrown when failing to open a directory.
+ */
+class DirectoryOpenFailed : public FilesystemError {
+  public:
+    DirectoryOpenFailed(const std::string &p, int ec = 0)
+        : FilesystemError("Failed to open directory: " + p, p, ec) {}
+};
+}; // namespace OS
