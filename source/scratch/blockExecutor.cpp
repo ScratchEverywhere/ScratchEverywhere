ab601f6be8bddda0a12cb692ae11369ab59c8742
NateXS <thecoolgamer0003@gmail.com>
2025-12-20 13:46:42 -0600

    [Runtime] Re-add Sprite names to monitors

--- source/scratch/blockExecutor.cpp
+++ source/scratch/blockExecutor.cpp
@@ -618,8 +618,11 @@ void BlockExecutor::updateMonitors() {
             if (var.opcode == "data_variable") {
                 var.value = BlockExecutor::getVariableValue(var.id, sprite);
                 var.displayName = Math::removeQuotations(var.parameters["VARIABLE"]);
+                if (!sprite->isStage) var.displayName = sprite->name + ": " + var.displayName;
             } else if (var.opcode == "data_listcontents") {
                 var.displayName = Math::removeQuotations(var.parameters["LIST"]);
+                if (!sprite->isStage) var.displayName = sprite->name + ": " + var.displayName;
+
                 // Check lists
                 auto listIt = sprite->lists.find(var.id);
                 if (listIt != sprite->lists.end())
