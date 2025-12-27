a68b3cc86c42d934386665a7e55ff90bbd4e304c
Rocco Phoenix-Morrison <roccophoenixmorrison@gmail.com>
2025-12-27 19:38:42 +0000

    clang-format on windows fix
    Initial commit made on Windows while testing, do not have clang-format installed on Windows

--- source/runtime/blocks/blockUtils.hpp
+++ source/runtime/blocks/blockUtils.hpp
@@ -32,8 +32,8 @@ BlockResult nopBlock(Block &block, Sprite *sprite, bool *withoutScreenRefresh, b
  * @sa SCRATCH_SHADOW_BLOCK
  * @sa BlockExecutor
  */
-#define SCRATCH_BLOCK(category, id)                                                                                               \
-    BlockResult block_##category##_##id##_(Block &block, Sprite *sprite, bool *withoutScreenRefresh, bool fromRepeat);            \
+#define SCRATCH_BLOCK(category, id)                                                                                                    \
+    BlockResult block_##category##_##id##_(Block &block, Sprite *sprite, bool *withoutScreenRefresh, bool fromRepeat);                 \
     static uint8_t block_##category##_##id##_reg_ = (BlockExecutor::getHandlers()[#category "_" #id] = block_##category##_##id##_, 0); \
     BlockResult block_##category##_##id##_(Block &block, Sprite *sprite, bool *withoutScreenRefresh, bool fromRepeat)
 
@@ -82,8 +82,8 @@ BlockResult nopBlock(Block &block, Sprite *sprite, bool *withoutScreenRefresh, b
  * @sa SCRATCH_SHADOW_BLOCK
  * @sa BlockExecutor
  */
-#define SCRATCH_REPORTER_BLOCK(category, id)                                                                                           \
-    Value block_##category##_##id##_(Block &block, Sprite *sprite);                                                                    \
+#define SCRATCH_REPORTER_BLOCK(category, id)                                                                                                \
+    Value block_##category##_##id##_(Block &block, Sprite *sprite);                                                                         \
     static uint8_t block_##category##_##id##_reg_ = (BlockExecutor::getValueHandlers()[#category "_" #id] = block_##category##_##id##_, 0); \
     Value block_##category##_##id##_(Block &block, Sprite *sprite)
 
@@ -109,8 +109,8 @@ BlockResult nopBlock(Block &block, Sprite *sprite, bool *withoutScreenRefresh, b
  * @sa SCRATCH_SHADOW_BLOCK
  * @sa BlockExecutor
  */
-#define SCRATCH_REPORTER_BLOCK_OPCODE(opcode)                                                              \
-    Value block_##opcode##_(Block &block, Sprite *sprite);                                                 \
+#define SCRATCH_REPORTER_BLOCK_OPCODE(opcode)                                                                   \
+    Value block_##opcode##_(Block &block, Sprite *sprite);                                                      \
     static uint8_t block_##opcode##_reg_ = (BlockExecutor::getValueHandlers()[#opcode] = block_##opcode##_, 0); \
     Value block_##opcode##_(Block &block, Sprite *sprite)
 
