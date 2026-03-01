d775b0b9461baabb2360aa572debad1342483edb
Grady Link <github@grady.link>
2026-02-28 19:43:36 -0800

    feat(headless): support new svg scaling stuff

--- source/renderers/headless/image_headless.hpp
+++ source/renderers/headless/image_headless.hpp
@@ -3,9 +3,9 @@
 
 class Image_Headless : public Image {
   public:
-    Image_Headless(std::string filePath, bool fromScratchProject = true, bool bitmapHalfQuality = false);
+    Image_Headless(std::string filePath, bool fromScratchProject = true, bool bitmapHalfQuality = false, float scale = 1);
 
-    Image_Headless(std::string filePath, mz_zip_archive *zip, bool bitmapHalfQuality = false);
+    Image_Headless(std::string filePath, mz_zip_archive *zip, bool bitmapHalfQuality = false, float scale = 1);
 
     ~Image_Headless() override;
 
@@ -13,4 +13,6 @@ class Image_Headless : public Image {
     void renderNineslice(double xPos, double yPos, double width, double height, double padding, bool centered = false) override;
 
     void *getNativeTexture() override;
+
+    void refreshTexture() override;
 };
