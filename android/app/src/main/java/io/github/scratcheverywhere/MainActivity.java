21dba427337cdadf676413d5f030fd9bc1732cc2
PwLDev <64767383+PwLDev@users.noreply.github.com>
2026-03-08 18:15:38 -0700

    feat(android): add jni bridge

--- android/app/src/main/java/io/github/scratcheverywhere/MainActivity.java
+++ android/app/src/main/java/io/github/scratcheverywhere/MainActivity.java
@@ -1,12 +1,22 @@
 package io.github.scratcheverywhere;
 
+import android.os.Bundle;
+
 import org.libsdl.app.SDLActivity;
 
 public class MainActivity extends SDLActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		nativeSetupJNI();
+	}
+
 	@Override
 	protected String[] getLibraries() {
 		return new String[] {
 			"scratch"   // Scratch Everywhere!
 		};
 	}
+
+	public static native int nativeSetupJNI();
 }
