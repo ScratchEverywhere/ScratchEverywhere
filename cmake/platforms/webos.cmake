2f697ec6e8eaac77ca11310a0d6775b70b1d5c03
Grady Link <github@grady.link>
2026-03-14 18:46:16 -0700

    feat(cmake): add webos platform config

--- cmake/platforms/webos.cmake
+++ cmake/platforms/webos.cmake
@@ -1,3 +1,22 @@
+set(SE_DEFAULT_OUTPUT_NAME "scratch-webos")
+
+set(SE_RENDERER_VALID_OPTIONS "sdl2")
+set(SE_AUDIO_ENGINE_VALID_OPTIONS "sdl2")
+set(SE_DEPS_VALID_OPTIONS "fallback" "system")
+
+set(SE_CACHING_DEFAULT ON)
+set(SE_CMAKERC_DEFAULT OFF)
+
+set(SE_ALLOW_CLOUDVARS OFF)
+set(SE_ALLOW_DOWNLOAD OFF)
+
+set(SE_HAS_THREADS ON)
+
+set(SE_HAS_TOUCH FALSE)
+set(SE_HAS_MOUSE TRUE)
+set(SE_HAS_KEYBOARD TRUE)
+set(SE_HAS_CONTROLLER TRUE)
+
 macro(package_platform)
 	# toolchain path: ~/arm-webos-linux-gnueabi_sdk-buildroot/share/buildroot/toolchainfile.cmake
 	target_link_options(scratch-everywhere PRIVATE "-static-libstdc++" "-static-libgcc")
