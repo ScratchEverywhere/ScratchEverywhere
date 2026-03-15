0210a30569916ccf3a0b60e0187bf8823e49d979
Grady Link <github@grady.link>
2026-03-14 18:41:38 -0700

    feat(cmake): add wii u platform config

--- cmake/platforms/wiiu.cmake
+++ cmake/platforms/wiiu.cmake
@@ -1,6 +1,22 @@
+set(SE_DEFAULT_OUTPUT_NAME "scratch-wiiu")
+
+set(SE_RENDERER_VALID_OPTIONS "sdl2")
+set(SE_AUDIO_ENGINE_VALID_OPTIONS "sdl2")
+set(SE_DEPS_VALID_OPTIONS "fallback" "system")
+
+set(SE_CACHING_DEFAULT ON)
+set(SE_CMAKERC_DEFAULT ON)
+
 set(SE_ALLOW_CLOUDVARS ON)
 set(SE_ALLOW_DOWNLOAD ON)
 set(SE_SYSTEM_CURL ON)
+
+set(SE_HAS_THREADS ON)
+
+set(SE_HAS_TOUCH TRUE)
+set(SE_HAS_MOUSE FALSE)
+set(SE_HAS_KEYBOARD FALSE)
+set(SE_HAS_CONTROLLER TRUE)
 
 macro(package_platform)
 	wut_create_rpx(scratch-everywhere)
