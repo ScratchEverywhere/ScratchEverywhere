8a0f71ebdf0878316c7dbc9c7238e464896b9887
Grady Link <github@grady.link>
2026-03-16 23:53:31 -0700

    fix(opengl): don't allow gles2
    we don't actually support it properly

--- cmake/renderers/opengl.cmake
+++ cmake/renderers/opengl.cmake
@@ -3,16 +3,8 @@ if(TARGET renderer_interface)
 endif()
 add_library(renderer_interface INTERFACE)
 
-option(SE_OPENGL_ES "Use OpenGL ES instead of OpenGL" OFF)
-
-if(SE_OPENGL_ES)
-	find_package(OpenGL REQUIRED COMPONENTS GLES2)
-	target_link_libraries(renderer_interface INTERFACE OpenGL::GLES2)
-	target_compile_definitions(renderer_interface INTERFACE USE_GLES)
-else()
-	find_package(OpenGL REQUIRED)
-	target_link_libraries(renderer_interface INTERFACE OpenGL::GL)
-endif()
+find_package(OpenGL REQUIRED)
+target_link_libraries(renderer_interface INTERFACE OpenGL::GL)
 
 set(SE_WINDOWING_VALID_OPTIONS "sdl2" "sdl1" "sdl3" "glfw")
 set(SE_AUDIO_ENGINE_DEFAULT "sdl2")
