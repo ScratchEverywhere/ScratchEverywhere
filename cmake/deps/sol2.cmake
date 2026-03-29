492b041ea0d0e42d7b5ec484911b33f001c8302b
Grady Link <github@grady.link>
2026-03-28 22:30:54 -0700

    fix(cmake): use wrapper for sol2

--- cmake/deps/sol2.cmake
+++ cmake/deps/sol2.cmake
@@ -1,25 +1,18 @@
 include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/deps/add_dependency.cmake")
 se_add_dependency(lua51)
-if(NOT TARGET lua51)
-	get_target_property(IS_ALIAS deps::sol2 ALIASED_TARGET)
-	if(IS_ALIAS)
-		add_library(lua51 ALIAS ${IS_ALIAS})
-	else()
-		add_library(lua51 ALIAS deps::lua51)
-	endif()
-endif()
 
 function(_dep_system_sol2)
 	find_package(sol2 CONFIG QUIET)
 endfunction()
 
-function(_dep_system_sol2)
+function(_dep_source_sol2)
 	CPMAddPackage(
 	  NAME sol2
 	  GITHUB_REPOSITORY ThePhD/sol2
 	  VERSION 3.3.0
-	  GIT_TAG develop
+	  GIT_TAG develop # Fixes sol::optional error
 	  OPTIONS "SOL2_ENABLE_INSTALL OFF"
 	)
-	target_link_libraries(sol2 INTERFACE deps::lua51)
+	add_library(deps::sol2 INTERFACE IMPORTED)
+    target_link_libraries(deps::sol2 INTERFACE sol2 deps::lua51)
 endfunction()
