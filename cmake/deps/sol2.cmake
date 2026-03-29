4e2db41443eee8a0362b9d22664a602081aa4a22
Grady Link <github@grady.link>
2026-03-28 22:16:33 -0700

    fix(cmake): sol2 -> lua5.1 target

--- cmake/deps/sol2.cmake
+++ cmake/deps/sol2.cmake
@@ -1,5 +1,13 @@
 include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/deps/add_dependency.cmake")
 se_add_dependency(lua51)
+if(NOT TARGET lua51)
+	get_target_property(IS_ALIAS deps::sol2 ALIASED_TARGET)
+	if(IS_ALIAS)
+		add_library(lua51 ALIAS ${IS_ALIAS})
+	else()
+		add_library(lua51 ALIAS deps::lua51)
+	endif()
+endif()
 
 function(_dep_system_sol2)
 	find_package(sol2 CONFIG QUIET)
