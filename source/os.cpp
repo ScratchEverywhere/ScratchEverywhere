023c3c12e9c1183f584fd62f36ae26f55abba93c
high on tantor <josephvenable@verizon.net>
2026-04-14 00:11:48 -0400

    Update os.cpp

--- source/os.cpp
+++ source/os.cpp
@@ -229,47 +229,46 @@ bool toExit = false;
 }
 
 std::string OS::getScratchFolderLocation() {
+    nlohmann::json json;
     if (!loadedSettings) {
         loadedSettings = true;
 
-        nlohmann::json json = SettingsManager::getConfigSettings();
+        json = SettingsManager::getConfigSettings();
 
-        if (json.contains("ProjectsPath") && json["ProjectsPath"].is_string() && json.contains("UseProjectsPath") && json["UseProjectsPath"].is_boolean() && json["UseProjectsPath"] == true) customProjectsPath = new std::string(json["ProjectsPath"].get<std::string>());
-    }
-
+        if (json.contains("ProjectsPath") && json["ProjectsPath"].is_string() && json.contains("UseProjectsPath") && json["UseProjectsPath"].is_boolean() && json["UseProjectsPath"] == true) {
+            customProjectsPath = new std::string(json["ProjectsPath"].get<std::string>());
+        } else {
 #ifdef NULLIFY_STDERR
-    static bool init;
-    if (!init) {
-        init = true;
-        std::vector<std::string> cmd = ngs::ps::cmdline_from_proc_id(ngs::ps::proc_id_from_self());
-        if (cmd.size() == 1) {
-            std::string exe = ngs::ps::exe_from_proc_id(ngs::ps::proc_id_from_self());
+            std::vector<std::string> cmd = ngs::ps::cmdline_from_proc_id(ngs::ps::proc_id_from_self());
+            if (cmd.size() == 1) {
+                std::string exe = ngs::ps::exe_from_proc_id(ngs::ps::proc_id_from_self());
 #ifdef _WIN32
-            std::size_t fp = exe.find_last_of("\\");
+                std::size_t fp = exe.find_last_of("\\");
 #else
-            std::size_t fp = exe.find_last_of("/");
+                std::size_t fp = exe.find_last_of("/");
 #endif
-            exe = exe.substr(0, fp + 1);
+                exe = exe.substr(0, fp + 1);
 #ifdef _WIN32
-            ngs::ps::NGS_PROCID pid = ngs::ps::spawn_child_proc_id(std::string("\"") + exe + std::string("external\\folderbrowser.exe\""), true);
+                ngs::ps::NGS_PROCID pid = ngs::ps::spawn_child_proc_id(std::string("\"") + exe + std::string("external\\folderbrowser.exe\""), true);
 #else
-            ngs::ps::NGS_PROCID pid = ngs::ps::spawn_child_proc_id(std::string("\"") + exe + std::string("external/folderbrowser\""), true);
+                ngs::ps::NGS_PROCID pid = ngs::ps::spawn_child_proc_id(std::string("\"") + exe + std::string("external/folderbrowser\""), true);
 #endif
-            if (pid > 0) {
-                static std::string result = ngs::ps::read_from_stdout_for_child_proc_id(pid);
-                ngs::ps::free_stdout_for_child_proc_id(pid);
-                ngs::ps::free_stdin_for_child_proc_id(pid);
-                std::size_t pos = result.find_first_of("\r\n");
-                if (pos != std::string::npos) {
-                    static std::string result_trimmed = result.substr(0, pos);
-                    customProjectsPath = &result_trimmed;
-                } else {
-                    customProjectsPath = &result;
+                if (pid > 0) {
+                    static std::string result = ngs::ps::read_from_stdout_for_child_proc_id(pid);
+                    ngs::ps::free_stdout_for_child_proc_id(pid);
+                    ngs::ps::free_stdin_for_child_proc_id(pid);
+                    std::size_t pos = result.find_first_of("\r\n");
+                    if (pos != std::string::npos) {
+                        static std::string result_trimmed = result.substr(0, pos);
+                        customProjectsPath = &result_trimmed;
+                    } else {
+                        customProjectsPath = &result;
+                    }
                 }
             }
+#endif
         }
     }
-#endif
 
     if (customProjectsPath != nullptr) return *customProjectsPath;
 
