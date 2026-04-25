047c577b06c68c562ff8bdec777fc3bfd6ec7743
Grady Link <github@grady.link>
2026-04-25 09:50:25 -0700

    feat(cmake): remove native extension prefix

--- cmake/add_extension.cmake
+++ cmake/add_extension.cmake
@@ -2,6 +2,7 @@ include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/deps/add_dependency.cmake")
 
 function(add_extension TARGET)
 	add_library(${TARGET} SHARED ${ARGN})
+	set_target_properties(${TARGET} PROPERTIES PREFIX "")
 	target_link_libraries(${TARGET} PRIVATE se-interface)
 
 	if(APPLE)
