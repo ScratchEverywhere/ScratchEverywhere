e7a0372ecf0452f06f3593df20d7c2ebc0b4292c
Nishi <nishi@nishi.boats>
2026-05-03 06:40:17 +0900

    [Libretro] Do not build GLFW for libretro

--- cmake/windowing/libretro.cmake
+++ cmake/windowing/libretro.cmake
@@ -2,6 +2,3 @@ if(TARGET windowing_interface)
     return()
 endif()
 add_library(windowing_interface INTERFACE)
-
-include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/deps/add_dependency.cmake")
-se_add_dependency(windowing_interface glfw)
