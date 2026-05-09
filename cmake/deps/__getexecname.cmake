bbb9c0d1ea2d3bb874775b6175e46dd2f9925a03
high on tantor <josephvenable@verizon.net>
2026-05-08 22:22:51 -0400

    Various critiques

--- cmake/deps/__getexecname.cmake
+++ cmake/deps/__getexecname.cmake
@@ -1,14 +1,4 @@
-function(_dep_system___getexecname)
-	find_path(__GETEXECNAME_INCLUDE_DIR 
-		NAMES __getexecname/internal.h __getexecname/internal.cpp
-		PATHS /usr/include /usr/include/__getexecname
-	)
-
-	if(__GETEXECNAME_INCLUDE_DIR)
-		add_library(__getexecname STATIC ${__GETEXECNAME_INCLUDE_DIR}/__getexecname/internal.cpp)
-		target_include_directories(__getexecname PUBLIC ${__GETEXECNAME_INCLUDE_DIR})
-	endif()
-endfunction()
+set(SE_FORCE_SOURCE___getexecname TRUE)
 
 function(_dep_source___getexecname)
 	if(NOT DEFINED __getexecname_SOURCE_DIR)
