c58e00c104138f1ba0d592d1c10bef5d70ed345e
high on tantor <josephvenable@verizon.net>
2026-07-14 23:50:01 -0400

    Update settingsMenu.cpp

--- source/menus/settingsMenu.cpp
+++ source/menus/settingsMenu.cpp
@@ -229,14 +229,14 @@ void SettingsMenu::render() {
     if (ChangeFolderPath->isPressed({"a"})) {
 #if defined(_WIN32) || defined(_WIN64) || defined(__APPLE__) || (defined(__linux__) && !defined(__ANDROID__)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
 		const std::string newPathGui = get_directory_alt("Select a custom path to load *.sb3 Scratch project files...", "");
-		const std::string newPath = ((newPathGui.empty()) ? Input::openSoftwareKeyboard(projectsPath.c_str()) : Input::openSoftwareKeyboard(newPathGui.c_str()));
+		const std::string newPath = ((newPathGui.empty()) ? Input::openSoftwareKeyboard(projectsPath.c_str()) : newPathGui);
 #else
 		const std::string newPath = Input::openSoftwareKeyboard(projectsPath.c_str());
 #endif
 
         if (newPath.length() > 0) {
             projectsPath = newPath;
-			OS::loadedSettings = false;
+
             updateButtonStates();
         }
     }
