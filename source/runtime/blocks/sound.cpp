cc026402eedee6303ff863b9583f87a5c0936467
NateXS <thecoolgamer0003@gmail.com>
2026-07-14 19:19:59 -0500

    [Runtime] Fix `Volume` blocks not working

--- source/runtime/blocks/sound.cpp
+++ source/runtime/blocks/sound.cpp
@@ -184,9 +184,6 @@ SCRATCH_BLOCK(sound, cleareffects) {
 }
 
 SCRATCH_BLOCK(sound, changevolumeby) {
-    BlockState *state = thread->getState(block);
-    if (state->completedSteps != 0) return BlockResult::CONTINUE;
-
     Value volume;
     if (!Scratch::getInput(block, "VOLUME", thread, sprite, volume)) return BlockResult::REPEAT;
 
@@ -195,15 +192,10 @@ SCRATCH_BLOCK(sound, changevolumeby) {
     for (Sound sound : sprite->sounds) {
         Mixer::setSoundVolume(sound.fullName, sprite->volume + inputValue);
     }
-
-    state->completedSteps = 1;
     return BlockResult::REPEAT;
 }
 
 SCRATCH_BLOCK(sound, setvolumeto) {
-    BlockState *state = thread->getState(block);
-    if (state->completedSteps != 0) return BlockResult::CONTINUE;
-
     Value volume;
     if (!Scratch::getInput(block, "VOLUME", thread, sprite, volume)) return BlockResult::REPEAT;
 
@@ -212,8 +204,6 @@ SCRATCH_BLOCK(sound, setvolumeto) {
         Mixer::setSoundVolume(sound.fullName, inputValue);
     }
     sprite->volume = inputValue;
-
-    state->completedSteps = 1;
     return BlockResult::REPEAT;
 }
 
