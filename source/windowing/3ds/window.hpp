6457835e437a730330476295a63c308698ff8ad4
high on tantor <josephvenable@verizon.net>
2026-07-15 01:49:09 -0400

    Update window.hpp

--- source/windowing/3ds/window.hpp
+++ source/windowing/3ds/window.hpp
@@ -4,6 +4,8 @@
 #include <citro3d.h>
 #include <window.hpp>
 
+// 'Window' is a typedef in <X11/Xlib.h>: use 'WindowSE' as class name to avoid conflicts!
+
 class Window3DS : public WindowSE {
   public:
     bool init(int width, int height, const std::string &title) override;
