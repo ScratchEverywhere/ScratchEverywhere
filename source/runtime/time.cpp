9bec11a8be9910f75b6e473ce0838e5af0878c5c
high on tantor <josephvenable@verizon.net>
2026-07-15 01:43:12 -0400

    Update time.cpp

--- source/runtime/time.cpp
+++ source/runtime/time.cpp
@@ -3,49 +3,49 @@
 #include <cstddef>
 #include <ctime>
 
-int Time::getHours() {
+int TimeSE::getHours() {
     const time_t unixTime = time(NULL);
     struct tm *timeStruct = localtime((const time_t *)&unixTime);
     return timeStruct->tm_hour;
 }
 
-int Time::getMinutes() {
+int TimeSE::getMinutes() {
     const time_t unixTime = time(NULL);
     struct tm *timeStruct = localtime((const time_t *)&unixTime);
     return timeStruct->tm_min;
 }
 
-int Time::getSeconds() {
+int TimeSE::getSeconds() {
     const time_t unixTime = time(NULL);
     const struct tm *timeStruct = localtime((const time_t *)&unixTime);
     return timeStruct->tm_sec;
 }
 
-int Time::getDay() {
+int TimeSE::getDay() {
     const time_t unixTime = time(NULL);
     const struct tm *timeStruct = localtime((const time_t *)&unixTime);
     return timeStruct->tm_mday;
 }
 
-int Time::getDayOfWeek() {
+int TimeSE::getDayOfWeek() {
     const time_t unixTime = time(NULL);
     const struct tm *timeStruct = localtime((const time_t *)&unixTime);
     return timeStruct->tm_wday + 1;
 }
 
-int Time::getMonth() {
+int TimeSE::getMonth() {
     const time_t unixTime = time(NULL);
     const struct tm *timeStruct = localtime((const time_t *)&unixTime);
     return timeStruct->tm_mon + 1;
 }
 
-int Time::getYear() {
+int TimeSE::getYear() {
     const time_t unixTime = time(NULL);
     const struct tm *timeStruct = localtime((const time_t *)&unixTime);
     return timeStruct->tm_year + 1900;
 }
 
-double Time::getDaysSince2000() {
+double TimeSE::getDaysSince2000() {
     const auto now = std::chrono::system_clock::now();
 
     struct tm start_tm = {.tm_sec = 0, .tm_min = 0, .tm_hour = 0, .tm_mday = 1, .tm_mon = 0, .tm_year = 2000 - 1900};
