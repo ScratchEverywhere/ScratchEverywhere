461df3a291835d09ccc8575c52a255cdb409b50d
high on tantor <josephvenable@verizon.net>
2026-07-15 01:49:38 -0400

    Update window.hpp

--- source/windowing/headless/window.hpp
+++ source/windowing/headless/window.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <window.hpp>
 
+// 'Window' is a typedef in <X11/Xlib.h>: use 'WindowSE' as class name to avoid conflicts!
+
 class WindowHeadless : public WindowSE {
   public:
     bool init(int width, int height, const std::string &title) override { return true; }
