0e28e6c07e07eb9c35aa94686559c74a8ddbd1c6
high on tantor <josephvenable@verizon.net>
2026-07-16 18:31:02 -0400

    [skip ci] Update headless.cmake

--- cmake/windowing/headless.cmake
+++ cmake/windowing/headless.cmake
@@ -4,6 +4,6 @@ endif()
 add_library(windowing_interface INTERFACE)
 
 if (APPLE)
-	find_library(CLANG_RT_OSX libclang_rt.osx.a PATHS "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang" "/Library/Developer/CommandLineTools/usr/lib/clang" PATH_SUFFIXES "lib/darwin")
+	find_library(CLANG_RT_OSX "libclang_rt.osx.a" PATHS "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang" "/Library/Developer/CommandLineTools/usr/lib/clang" PATH_SUFFIXES "lib/darwin")
     target_link_libraries(windowing_interface INTERFACE "${CLANG_RT_OSX}")
 endif()
