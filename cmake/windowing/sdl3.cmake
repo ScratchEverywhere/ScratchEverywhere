b5739809150e148e6afb46fcaa14a6df8e04bfa9
high on tantor <josephvenable@verizon.net>
2026-07-16 18:36:49 -0400

    Update sdl3.cmake

--- cmake/windowing/sdl3.cmake
+++ cmake/windowing/sdl3.cmake
@@ -5,8 +5,3 @@ add_library(windowing_interface INTERFACE)
 
 include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/deps/add_dependency.cmake")
 se_add_dependency(windowing_interface SDL3)
-
-if (APPLE)
-	find_library(CLANG_RT_OSX "libclang_rt.osx.a" PATHS "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang" "/Library/Developer/CommandLineTools/usr/lib/clang" PATH_SUFFIXES "lib/darwin")
-    target_link_libraries(windowing_interface INTERFACE "${CLANG_RT_OSX}")
-endif()
