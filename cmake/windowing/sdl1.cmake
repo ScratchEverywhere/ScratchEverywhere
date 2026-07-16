d9b4babc89a4aa38c92356a9be44d6708423f65c
high on tantor <josephvenable@verizon.net>
2026-07-16 18:02:12 -0400

    [skip ci] Update sdl1.cmake

--- cmake/windowing/sdl1.cmake
+++ cmake/windowing/sdl1.cmake
@@ -3,10 +3,10 @@ if(TARGET windowing_interface)
 endif()
 add_library(windowing_interface INTERFACE)
 
-if (APPLE)
-	execute_process(COMMAND clang "--print-runtime-dir" OUTPUT_VARIABLE CLANG_COMMAND_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
-	target_link_libraries(libimhex PUBLIC "${CLANG_COMMAND_OUTPUT}/libclang_rt.osx.a")
-endif()
-
 include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/deps/add_dependency.cmake")
 se_add_dependency(windowing_interface SDL)
+
+if (APPLE)
+	execute_process(COMMAND clang "--print-runtime-dir" OUTPUT_VARIABLE CLANG_COMMAND_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
+	target_link_libraries(windowing_interface PUBLIC "${CLANG_COMMAND_OUTPUT}/libclang_rt.osx.a")
+endif()
