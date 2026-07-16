142a4513a619723f9113a6496b320821d0e821ab
high on tantor <josephvenable@verizon.net>
2026-07-16 18:03:53 -0400

    Update headless.cmake

--- cmake/windowing/headless.cmake
+++ cmake/windowing/headless.cmake
@@ -2,3 +2,8 @@ if(TARGET windowing_interface)
     return()
 endif()
 add_library(windowing_interface INTERFACE)
+
+if (APPLE)
+	execute_process(COMMAND clang "--print-runtime-dir" OUTPUT_VARIABLE CLANG_COMMAND_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
+	target_link_libraries(windowing_interface PUBLIC "${CLANG_COMMAND_OUTPUT}/libclang_rt.osx.a")
+endif()
