766281d3f85e0942cd5eef55b1a54d62f32ee753
high on tantor <josephvenable@verizon.net>
2026-07-16 18:03:31 -0400

    [skip ci] Update glfw.cmake

--- cmake/windowing/glfw.cmake
+++ cmake/windowing/glfw.cmake
@@ -5,3 +5,8 @@ add_library(windowing_interface INTERFACE)
 
 include("${CMAKE_CURRENT_SOURCE_DIR}/cmake/deps/add_dependency.cmake")
 se_add_dependency(windowing_interface glfw)
+
+if (APPLE)
+	execute_process(COMMAND clang "--print-runtime-dir" OUTPUT_VARIABLE CLANG_COMMAND_OUTPUT OUTPUT_STRIP_TRAILING_WHITESPACE)
+	target_link_libraries(windowing_interface PUBLIC "${CLANG_COMMAND_OUTPUT}/libclang_rt.osx.a")
+endif()
