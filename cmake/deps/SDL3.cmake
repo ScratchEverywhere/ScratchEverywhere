b0558137538b81e2f8654af9f89e097d4dd883ea
high on tantor <josephvenable@verizon.net>
2026-07-17 17:02:41 -0400

    Update SDL3.cmake

--- cmake/deps/SDL3.cmake
+++ cmake/deps/SDL3.cmake
@@ -13,3 +13,18 @@ function(_dep_source_SDL3)
 		OPTIONS "SDL_TEST_LIBRARY OFF"
 	)
 endfunction()
+
+# compiler
+if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND NOT WEBOS)
+	target_compile_definitions(SDL3 PUBLIC USE_SDL3_POLLEVENT)
+elseif(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
+	target_compile_definitions(SDL3 PUBLIC USE_SDL3_POLLEVENT)
+elseif(CMAKE_SYSTEM_NAME STREQUAL "DragonFly")
+	target_compile_definitions(SDL3 PUBLIC USE_SDL3_POLLEVENT)
+elseif(CMAKE_SYSTEM_NAME STREQUAL "NetBSD")
+	target_compile_definitions(SDL3 PUBLIC USE_SDL3_POLLEVENT)
+elseif(CMAKE_SYSTEM_NAME STREQUAL "OpenBSD")
+	target_compile_definitions(SDL3 PUBLIC USE_SDL3_POLLEVENT)
+elseif(CMAKE_SYSTEM_NAME STREQUAL "SunOS")
+	target_compile_definitions(SDL3 PUBLIC USE_SDL3_POLLEVENT)
+endif()
