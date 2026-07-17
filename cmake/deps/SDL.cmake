dcbfa238627ff5512f0f9fc755dad9345deb6033
high on tantor <josephvenable@verizon.net>
2026-07-17 18:15:53 -0400

    [skip ci] Update SDL.cmake

--- cmake/deps/SDL.cmake
+++ cmake/deps/SDL.cmake
@@ -8,18 +8,3 @@ function(_dep_source_SDL)
 		"SDL doesn't support building from source."
 	)
 endfunction()
-
-# compiler
-if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND NOT WEBOS)
-	target_compile_definitions(SDL PUBLIC USE_SDL_POLLEVENT)
-elseif(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
-	target_compile_definitions(SDL PUBLIC USE_SDL_POLLEVENT)
-elseif(CMAKE_SYSTEM_NAME STREQUAL "DragonFly")
-	target_compile_definitions(SDL PUBLIC USE_SDL_POLLEVENT)
-elseif(CMAKE_SYSTEM_NAME STREQUAL "NetBSD")
-	target_compile_definitions(SDL PUBLIC USE_SDL_POLLEVENT)
-elseif(CMAKE_SYSTEM_NAME STREQUAL "OpenBSD")
-	target_compile_definitions(SDL PUBLIC USE_SDL_POLLEVENT)
-elseif(CMAKE_SYSTEM_NAME STREQUAL "SunOS")
-	target_compile_definitions(SDL PUBLIC USE_SDL_POLLEVENT)
-endif()
