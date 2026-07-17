7256f658757bfe08316e5f834c20f1a53504372e
high on tantor <josephvenable@verizon.net>
2026-07-17 18:16:39 -0400

    [skip ci] Update SDL2.cmake

--- cmake/deps/SDL2.cmake
+++ cmake/deps/SDL2.cmake
@@ -31,18 +31,3 @@ function(_dep_source_SDL2)
 		OPTIONS ${SDL2_OPTIONS}
 	)
 endfunction()
-
-# compiler
-if(CMAKE_SYSTEM_NAME STREQUAL "Linux" AND NOT WEBOS)
-	target_compile_definitions(SDL2 PUBLIC USE_SDL2_POLLEVENT)
-elseif(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD")
-	target_compile_definitions(SDL2 PUBLIC USE_SDL2_POLLEVENT)
-elseif(CMAKE_SYSTEM_NAME STREQUAL "DragonFly")
-	target_compile_definitions(SDL2 PUBLIC USE_SDL2_POLLEVENT)
-elseif(CMAKE_SYSTEM_NAME STREQUAL "NetBSD")
-	target_compile_definitions(SDL2 PUBLIC USE_SDL2_POLLEVENT)
-elseif(CMAKE_SYSTEM_NAME STREQUAL "OpenBSD")
-	target_compile_definitions(SDL2 PUBLIC USE_SDL2_POLLEVENT)
-elseif(CMAKE_SYSTEM_NAME STREQUAL "SunOS")
-	target_compile_definitions(SDL2 PUBLIC USE_SDL2_POLLEVENT)
-endif()
