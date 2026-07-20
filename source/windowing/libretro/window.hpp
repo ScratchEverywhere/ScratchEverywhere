b6a12352354b35ea792eb3860ae9e9efb0002f1c
high on tantor <josephvenable@verizon.net>
2026-07-20 17:19:33 -0400

    [skip ci] Update window.hpp

--- source/windowing/libretro/window.hpp
+++ source/windowing/libretro/window.hpp
@@ -1,8 +1,6 @@
 #pragma once
 #include <window.hpp>
 
-// 'Window' is a typedef in <X11/Xlib.h>: use 'WindowSE' as class name to avoid conflicts!
-
 class WindowLibretro : public WindowSE {
   public:
     bool init(int width, int height, const std::string &title) override;
