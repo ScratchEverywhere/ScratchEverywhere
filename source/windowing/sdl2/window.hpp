667b26143c44be38e6a97b5c7b57f86c6db3f16d
high on tantor <josephvenable@verizon.net>
2026-07-20 17:20:41 -0400

    [skip ci] Update window.hpp

--- source/windowing/sdl2/window.hpp
+++ source/windowing/sdl2/window.hpp
@@ -2,8 +2,6 @@
 #include <SDL.h>
 #include <window.hpp>
 
-// 'Window' is a typedef in <X11/Xlib.h>: use 'WindowSE' as class name to avoid conflicts!
-
 class WindowSDL2 : public WindowSE {
   public:
     bool init(int width, int height, const std::string &title) override;
