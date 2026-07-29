8fa62c4d416cfed8463364ffbc65ba56936552d0
high on tantor <josephvenable@verizon.net>
2026-07-28 22:45:17 -0400

    Update settingsMenu.cpp

--- source/menus/settingsMenu.cpp
+++ source/menus/settingsMenu.cpp
@@ -5,6 +5,11 @@
 #include "translation.hpp"
 #if defined(_WIN32) || defined(_WIN64) || defined(__APPLE__) || (defined(__linux__) && !defined(__ANDROID__) && !defined(WEBOS)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
 #include <libdlgmod/libdlgmod.h>
+#if (defined(__linux__) && !defined(__ANDROID__) && !defined(WEBOS)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
+#include <cstdlib>
+#include <algorithm>
+#include <sstream>
+#endif
 #if !defined(USE_LIBDLGMOD)
 #define USE_LIBDLGMOD
 #endif
