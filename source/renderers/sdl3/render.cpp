5204f64ef834fa41babd1df6ab4225a42af994bb
André Guilherme <75867486+Wolf3s@users.noreply.github.com>
2026-08-02 08:29:13 -0300

    Fix PS2 preprocessor.

--- source/renderers/sdl3/render.cpp
+++ source/renderers/sdl3/render.cpp
@@ -54,7 +54,7 @@ bool Render::Init() {
 #elif defined(VITA)
     int windowWidth = 960;
     int windowHeight = 544;
-#elif defined(PS2)
+#elif defined(__PS2__)
     int windowWidth = 640;
     int windowHeight = 448;
 #else
