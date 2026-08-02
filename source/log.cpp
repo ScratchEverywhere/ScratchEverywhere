89ed9dad6400ab0d180e92202d99b05b7fd174b3
Grady Link <github@grady.link>
2026-08-01 18:48:42 -0700

    fix: make log error a warning

--- source/log.cpp
+++ source/log.cpp
@@ -155,7 +155,7 @@ void Log::writeToFile(std::string message) {
 void Log::deleteLogFile() {
     std::string filePath = OS::getScratchFolderLocation() + "/log.txt";
     if (std::remove(filePath.c_str()) != 0) {
-        Log::logCritical("Failed to delete log file: " + std::string(std::strerror(errno)), false);
+        Log::logWarning("Failed to delete log file: " + std::string(std::strerror(errno)));
     }
 }
 #endif
