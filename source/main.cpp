f09f166722419a7e866450a6229607312078fd31
high on tantor <josephvenable@verizon.net>
2026-08-04 23:36:35 -0400

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -57,7 +57,7 @@ extern "C" __declspec(dllexport) const char *scratch_everywhere_step() {
 extern "C" __attribute__((visibility("default"))) const char *scratch_everywhere_step() {
 #endif
 	static char buffer[4];
-	std::pair<bool, bool> result = stepScratchProject(monitorDisplayThread);
+	std::pair<bool, bool> result = Scratch::stepScratchProject(monitorDisplayThread);
 	int first  = result.first  ? 1 : 0;
 	int second = result.second ? 1 : 0;
     snprintf(buffer, sizeof(buffer), "%d:%d", first, second);
