9ad2192b429359692a3ce3de48f5253f477fd6f6
high on tantor <josephvenable@verizon.net>
2026-08-12 21:51:59 -0400

    [skip ci] Update window.cpp

--- source/windowing/sdl2/window.cpp
+++ source/windowing/sdl2/window.cpp
@@ -1,5 +1,5 @@
 #include "window.hpp"
-#if defined(_WIN32) || defined(_WIN64) || defined(__APPLE__) || ((defined(__linux__) && !defined(__ANDROID__) && !defined(WEBOS)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
+#if defined(_WIN32) || defined(_WIN64) || defined(__APPLE__) || (defined(__linux__) && !defined(__ANDROID__) && !defined(WEBOS)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
 #include <SDL_syswm.h>
 #include <libdlgmod/libdlgmod.h>
 #include <cstring>
@@ -105,7 +105,7 @@ bool WindowSDL2::init(int width, int height, const std::string &title) {
     resize(dw, dh);
 #endif
 
-#if defined(_WIN32) || defined(_WIN64) || defined(__APPLE__) || ((defined(__linux__) && !defined(__ANDROID__) && !defined(WEBOS)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
+#if defined(_WIN32) || defined(_WIN64) || defined(__APPLE__) || (defined(__linux__) && !defined(__ANDROID__) && !defined(WEBOS)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
     SDL_SysWMinfo system_info;
     SDL_VERSION(&system_info.version);
     SDL_GetWindowWMInfo(window, &system_info);
