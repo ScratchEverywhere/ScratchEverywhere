6b165bafdaccb6a2839d04b8dc196c88a0a1573b
high on tantor <josephvenable@verizon.net>
2026-08-12 21:50:57 -0400

    Update window.cpp

--- source/windowing/glfw/window.cpp
+++ source/windowing/glfw/window.cpp
@@ -56,7 +56,7 @@ bool WindowGLFW::init(int w, int h, const std::string &title) {
     widget_set_owner(std::to_string((unsigned long long)(void *)glfwGetWin32Window(window)).c_str());
 #elif defined(__APPLE__)
     widget_set_owner(std::to_string((unsigned long long)(void *)glfwGetCocoaWindow(window)).c_str());
-#elif ((defined(__linux__) && !defined(__ANDROID__) && !defined(WEBOS)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
+#elif (defined(__linux__) && !defined(__ANDROID__) && !defined(WEBOS)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
     int platform = glfwGetPlatform();
 	if (platform == GLFW_PLATFORM_X11) {
 		widget_set_owner(std::to_string((unsigned long long)(unsigned long)glfwGetX11Window(window)).c_str());
