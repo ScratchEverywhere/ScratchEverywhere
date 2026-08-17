9ab6d6e10cc13f0c91d3f62b856a1ec3210125e6
high on tantor <josephvenable@verizon.net>
2026-08-17 04:24:54 -0400

    Update window.hpp

--- source/windowing/nds/window.hpp
+++ source/windowing/nds/window.hpp
@@ -5,7 +5,7 @@
 
 class WindowNDS : public WindowSE {
   public:
-    bool init(int width, int height, const std::string &title) override;
+    bool init(int width, int height, bool resizable, const std::string &title) override;
     void cleanup() override;
 
     bool shouldClose() override;
