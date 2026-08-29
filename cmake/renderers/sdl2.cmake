97fa878878787b8be1a6b700b4df4a8d9b8478fa
gradylink <github@grady.link>
2026-08-28 21:31:49 -0700

    fix(psp): pthread error

--- cmake/renderers/sdl2.cmake
+++ cmake/renderers/sdl2.cmake
@@ -6,6 +6,14 @@ add_library(renderer_interface INTERFACE)
 cl_add_dep(renderer_interface SDL2)
 cl_add_dep(renderer_interface SDL2_ttf)
 
+if(PSP AND TARGET PkgConfig::SDL2_ttf)
+	get_target_property(_SDL2_TTF_LINK_OPTS PkgConfig::SDL2_ttf INTERFACE_LINK_OPTIONS)
+	if(_SDL2_TTF_LINK_OPTS)
+		list(REMOVE_ITEM _SDL2_TTF_LINK_OPTS "-pthread")
+		set_target_properties(PkgConfig::SDL2_ttf PROPERTIES INTERFACE_LINK_OPTIONS "${_SDL2_TTF_LINK_OPTS}")
+	endif()
+endif()
+
 set(SE_WINDOWING_VALID_OPTIONS "sdl2")
 
 if(NOT DEFINED SE_AUDIO_ENGINE_DEFAULT)
