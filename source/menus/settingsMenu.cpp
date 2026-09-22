b3b8946092195b7b1909f97a836892d953227877
high on tantor <josephvenable@verizon.net>
2026-09-21 23:24:45 -0400

    make things less complicated

--- source/menus/settingsMenu.cpp
+++ source/menus/settingsMenu.cpp
@@ -252,15 +252,10 @@ void SettingsMenu::render() {
 
 #if defined(_WIN32) || defined(_WIN64) || defined(__APPLE__)
 
+        std::string newPathGui = get_directory_alt(folder_picker_dialog_titlebar_caption, "");
+
 #if defined(_WIN32) || defined(_WIN64)
-
-		std::string newPathGui = get_directory_alt(folder_picker_dialog_titlebar_caption, "");
 		std::replace(newPathGui.begin(), newPathGui.end(), '\\', '/'); // Normalize path separators
-
-#elif defined(__APPLE__)
-
-		std::string newPathGui = get_directory_alt(folder_picker_dialog_titlebar_caption, "");
-
 #endif
 
         const std::string newPath = ((newPathGui.empty()) ? projectsPath : newPathGui);
