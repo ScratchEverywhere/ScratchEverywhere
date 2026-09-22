e1f279a54a3d8f5408237f485dd7a4b3249744d0
high on tantor <josephvenable@verizon.net>
2026-09-21 22:28:10 -0400

    Update settingsMenu.cpp

--- source/menus/settingsMenu.cpp
+++ source/menus/settingsMenu.cpp
@@ -7,7 +7,7 @@
 #include <libdlgmod/libdlgmod.h>
 #if defined(_WIN32) || defined(_WIN64)
 #include <algorithm>
-#enduf
+#endif
 #if (defined(__linux__) && !defined(__ANDROID__) && !defined(WEBOS) && !defined(LIBRETRO)) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__) || (defined(__sun) && defined(__SVR4))
 #include <algorithm>
 #include <climits>
